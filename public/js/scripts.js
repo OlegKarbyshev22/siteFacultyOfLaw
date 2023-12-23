7372f1b1a29a8d128967234981f729f4db8869e2
LordofCreepers <vip.panzarhero173z@yandex.ru>
2023-12-23 18:03:14 +0400

    Закинул файлы в public

---
+++ public/js/scripts.js
@@ -0,0 +1,16 @@
+$('.owl-carousel').owlCarousel({
+    
+    margin:10,
+    dots: false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:3
+        }
+    }
+})
