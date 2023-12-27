699a65f6a6bc789d27042e1da11c69f77e9e7668
LordofCreepers <vip.panzarhero173z@yandex.ru>
2023-12-27 23:17:33 +0400

    Сделал "сеятель" для новостей, с помощью которого можно быстро создать несколько экземпляров новостей для тестирования

---
+++ database/seeders/NewsSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\News;
+use Illuminate\Database\Seeder;
+
+class NewsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        News::factory()->create([
+			"status" => "rejected"
+		]);
+    }
+}
