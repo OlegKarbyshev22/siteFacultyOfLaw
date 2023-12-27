97835be0966e8ad31714770a9a66aaea35fa29a2
LordofCreepers <vip.panzarhero173z@yandex.ru>
2023-12-27 21:21:53 +0400

    Исправил ссылки

--- resources/views/inc/header.blade.php
+++ resources/views/inc/header.blade.php
@@ -7,12 +7,12 @@
                 <div class="header__nav">
                     <nav>
                         <ul>
-                            <li><a class="header__link" href="#law" onclick="">Юридическое образование Ульяновской области в контексте времени</a></li>
-                            <li><a class="header__link" href="#challengesNewAge">Вызовы нового времени</a></li>
-                            <li><a class="header__link" href="">Книга памяти</a></li>
-                            <li><a class="header__link" href="#gallery_glorious_names">Галерея славных имён</a></li>
-                            <li><a class="header__link" href="#gallery_SVO">Участники СВО</a></li>
-                            <li><a class="header__link" href="#insert_news">Предложить новость +</a></li>
+                            <li><a class="header__link" href="/legalEducation" onclick="">Юридическое образование Ульяновской области в контексте времени</a></li>
+                            <li><a class="header__link" href="/challenges_new_age">Вызовы нового времени</a></li>
+                            <li><a class="header__link" href="/memorial_book">Книга памяти</a></li>
+                            <li><a class="header__link" href="/gallery_glorious_names">Галерея славных имён</a></li>
+                            <li><a class="header__link" href="/participants_SVO">Участники СВО</a></li>
+                            <li><a class="header__link" href="/send_news">Предложить новость +</a></li>
                         </ul>
                     </nav>
                 </div>
