a461ba13459800c3505c56d1234690ae39e0a3d5
Oleg Karbyshev <legys22@gmail.com>
2024-02-03 19:50:33 +0400

    Добавление открытия картинок в галереях и отобпражения описания

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,5 +1,5 @@
 $('.owl-carousel').owlCarousel({
-    
+
     margin:10,
     dots: false,
     responsive:{
@@ -13,4 +13,25 @@ $('.owl-carousel').owlCarousel({
             items:3
         }
     }
-})
+});
+
+document.querySelectorAll('.image-container .globalImage').forEach(imageContainer => {
+    imageContainer.onclick = () => {
+        const popup = document.querySelector('.popup-image');
+        const popupFIO = popup.querySelector('.popup-fio');
+        const fio = imageContainer.getAttribute('data-fio');
+
+        const popupDescription = popup.querySelector('.popup-description');
+        const description = imageContainer.getAttribute('data-description');
+
+        popup.style.display = 'block';
+        popupFIO.textContent = fio;
+        popupDescription.textContent = description;
+
+        popup.querySelector('img').src = imageContainer.querySelector('img').getAttribute('src');
+    }
+});
+
+document.querySelector('.popup-image span').onclick = () => {
+    document.querySelector('.popup-image').style.display = 'none';
+}
